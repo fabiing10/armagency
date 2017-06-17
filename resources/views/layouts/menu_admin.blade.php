7411eb89b0365dc5b5ba8d38ad20d5b46c7b5cbe
Fabian Zapata <fabian@g12media.net>
2017-06-17 15:00:47 -0500

    Views update 3

--- resources/views/layouts/menu_admin.blade.php
+++ resources/views/layouts/menu_admin.blade.php
@@ -5,13 +5,14 @@
           <div class="sidebar-head">
               <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu" style="padding-left: 20px;">Navigation</span></h3> </div>
               <ul class="nav" id="side-menu">
-                  <li> <a href="/admin/" class="waves-effect px-25"><i class="fa fa-list"></i> <span class="hide-menu" style="padding-left: 20px;"> Summary <span></span></span></a>
+                  <li>
+                    <a href="/admin/" class="waves-effect px-25"><i class="fa fa-list"></i> <span class="hide-menu" style="padding-left: 20px;"> Summary <span></span></span></a>
                   </li>
-                  <li> <a href="/admin/active-inactive" class="waves-effect px-25"><i class="fa fa-power-off"></i><span class="hide-menu" style="padding-left: 25px;">Status <span></span></span></a>
+                  <li><a href="/admin/create-certificate" class="waves-effect px-25"><i class="fa fa-plus-circle"></i> <span class="hide-menu" style="padding-left: 20px;"> New Client </span></a>
                   </li>
                   <li> <a href="/admin/alerts/" class="waves-effect px-25"><i class="fa fa-bell"></i> <span class="hide-menu" style="padding-left: 20px;"> Alerts <span></span></span></a>
                   </li>
-                  <li><a href="/admin/create-certificate" class="waves-effect px-25"><i class="fa fa-plus-circle"></i> <span class="hide-menu" style="padding-left: 20px;"> Create new </span></a>
+                  <li> <a href="/admin/active-inactive" class="waves-effect px-25"><i class="fa fa-power-off"></i><span class="hide-menu" style="padding-left: 25px;">Status <span></span></span></a>
                   </li>
                   <li><a href="/admin/admin-settings" class="waves-effect px-25"><i class="fa fa-key"></i> <span class="hide-menu" style="padding-left: 20px;"> Admin settings </span></a>
                   </li>
